b3f838ffa048208e4e5e7922c78e3d47bfa53ab9
Darmen Amanbayev <darmen.amanbayev@gmail.com>
2023-12-12 22:40:51 +0100

    y2023 d12 p2

--- src/y2023/d12/p2/solution.kt
+++ src/y2023/d12/p2/solution.kt
@@ -5,79 +5,51 @@ import readInput
 import utils.Memo
 import utils.memoize
 
-fun main() {
-    val input = readInput()
+data class Key(val places: List<Char>, val groups: List<Int>)
 
+val f = Memo<Key, Long>::process.memoize()
+
+fun Memo<Key, Long>.process(key: Key): Long {
+    val places = key.places
+    val groups = key.groups
+
+    if (places.isEmpty()) return if (groups.isEmpty()) 1 else 0
+    if (groups.isEmpty()) return if (places.contains('#')) 0 else 1
+
+    val fP = places.first()
+    val fG = groups.first()
+
+    val cLeft =
+        if (fP != '#') f(Key(places.drop(1), groups)) else 0
+
+    val cRight = when {
+        fG > places.size -> 0
+        places.take(fG).any { it == '.' } -> 0
+        places.size != fG && places[fG] == '#' -> 0
+        fP == '.' -> 0
+        else -> f(Key(places.drop(fG + 1), groups.drop(1)))
+    }
+
+    return cLeft + cRight
+}
+
+fun main() {
     var res = 0L
 
+    val input = readInput()
+
     for (i in input.indices) {
-        var count = 0L
+        val places = List(5) { _ -> input[i].split(" ").first() }
+            .joinToString("?")
+            .toList()
 
-        val places = List(5) { input[i].split(" ").first() }.joinToString(",")
-
-        val numbers = List(5) { input[i].split(" ").last() }
+        val numbers = List(5) { _ -> input[i].split(" ").last() }
             .joinToString(",")
             .split(",")
-            .map { it.toInt() }
+            .map { s -> s.toInt() }
 
-        val acc = Memo<Pair<Int, Int>, List<IntArray>>::process
-            .memoize()
-            .invoke(
-                places.length - numbers.sum() to numbers.size + 1
-            )
-            .filter {
-                !it.slice(1..it.size - 2).contains(0)
-            }
-
-        for (j in acc.indices) {
-            val s = acc[j]
-            var t = ""
-            for (k in s.indices) {
-                val a = acc[j][k]
-                t += ".".repeat(a)
-                if (k < numbers.size) t += "#".repeat(numbers[k])
-            }
-
-
-            var c = 0
-            for (l in t.indices) {
-                val place = if (places[l] == '?') t[l] else places[l]
-                if (place == t[l]) {
-                    c++
-                }
-            }
-
-            if (c == t.length) {
-                count++
-                println(t)
-            }
-        }
-
-        res += count
+        res += f(Key(places, numbers))
     }
 
     res.println()
-}
-
-fun Memo<Pair<Int, Int>, List<IntArray>>.process(key: Pair<Int, Int>): List<IntArray> {
-    val distributions = mutableListOf<IntArray>()
-
-    val n = key.first
-    val p = key.second
-
-    if (p == 0) {
-        if (n == 0) {
-            distributions.add(IntArray(0))
-        }
-        return distributions
-    }
-
-    for (i in 0..n) {
-        val subDistributions = recurse(n - i to p - 1)
-        for (subDistribution in subDistributions) {
-            distributions.add(intArrayOf(i) + subDistribution)
-        }
-    }
-
-    return distributions
 }
