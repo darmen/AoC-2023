493cf7d02b9137fada19157846d33bfa23f52772
Darmen Amanbayev <darmen.amanbayev@gmail.com>
2023-12-12 22:45:24 +0100

    Change template

---
+++ src/yXXXX/dX/p1/solution.kt
@@ -0,0 +1,16 @@
+package yXXXX.dX.p1
+
+import println
+import readInput
+
+fun main() {
+    val input = readInput()
+
+    var res = 0L
+
+    for (i in input.indices) {
+        val s = input[i]
+    }
+
+    res.println()
+}
