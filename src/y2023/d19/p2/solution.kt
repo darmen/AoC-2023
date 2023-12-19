c27b6985f8ce1e5560872dcb65fa4d2bed29b295
Darmen Amanbayev <darmen.amanbayev@gmail.com>
2023-12-19 17:41:59 +0100

    y2023 d19 p2 attempt

---
+++ src/y2023/d19/p2/solution.kt
@@ -0,0 +1,100 @@
+package y2023.d19.p2
+
+import Point
+import println
+import readInput
+import runMeasure
+import java.util.Stack
+
+sealed interface Condition
+
+class NormalCondition(val param: Char, val op: Char, val value: Int, val target: String) : Condition {
+    fun evaluate(): Int {
+        return if (op == '<') {
+            value - 1
+        } else {
+            4000 - value
+        }
+    }
+}
+
+class Pointer(val target: String) : Condition
+
+fun solve() {
+    val input = readInput()
+        .takeWhile { it != "" }
+        .associate {
+            val w = it
+                .dropLast(1)
+                .split("{")
+
+            val q = w.last()
+                .split(",")
+                .map { c ->
+                    if (c.contains(':')) {
+                        val p = c.split(':')
+                        NormalCondition(
+                            param = p.first().first(),
+                            op = p.first()[1],
+                            value = p.first().drop(2).takeWhile { it != ':' }.toInt(),
+                            target = p.last()
+                        )
+                    } else {
+                        Pointer(c)
+                    }
+                }
+
+            w.first() to q
+        }
+
+    var res = 0L
+
+    val visited = mutableSetOf<Condition>()
+    val stack = Stack<String>()
+    stack.push("in" )
+
+    while (stack.isNotEmpty()) {
+        val item = stack.peek()
+        for (c in input[item]!!) {
+            if (c is NormalCondition && c.target == "A") {
+                // inc
+                continue
+            }
+
+            if (c is Pointer && c.target == "A") {
+                // inc
+                continue
+            }
+
+
+            if (c is NormalCondition && c.target == "R") {
+                continue
+            }
+
+            if (c is Pointer && c.target == "R") {
+                continue
+            }
+
+            if (c in visited) {
+                continue
+            }
+
+            visited.add(c)
+
+            stack.push(
+                when (c) {
+                    is NormalCondition -> c.target
+                    is Pointer -> c.target
+                }
+            )
+
+            break
+        }
+    }
+
+    res.println()
+}
+
+fun main() {
+    runMeasure { solve() }
+}
