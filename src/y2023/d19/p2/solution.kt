6ff34c6a569728593590df810b5b90e25d4adf3c
Darmen Amanbayev <darmen.amanbayev@gmail.com>
2023-12-19 23:23:31 +0100

    Finally made it for y2023 d19 p2 (refactored a lot and changed approach)

--- src/y2023/d19/p2/solution.kt
+++ src/y2023/d19/p2/solution.kt
@@ -1,98 +1,100 @@
 package y2023.d19.p2
 
-import Point
 import println
 import readInput
 import runMeasure
-import java.util.Stack
+import kotlin.math.max
+import kotlin.math.min
 
 sealed interface Condition
+class Rule(val param: Char, val op: Char, val value: Long, val target: String) : Condition
 
-class NormalCondition(val param: Char, val op: Char, val value: Int, val target: String) : Condition {
-    fun evaluate(): Int {
-        return if (op == '<') {
-            value - 1
-        } else {
-            4000 - value
-        }
-    }
-}
-
-class Pointer(val target: String) : Condition
+class Fallback(val target: String) : Condition
 
 fun solve() {
     val input = readInput()
         .takeWhile { it != "" }
-        .associate {
-            val w = it
+        .associate { l ->
+            val (name, rest) = l
                 .dropLast(1)
                 .split("{")
 
-            val q = w.last()
-                .split(",")
-                .map { c ->
-                    if (c.contains(':')) {
-                        val p = c.split(':')
-                        NormalCondition(
-                            param = p.first().first(),
-                            op = p.first()[1],
-                            value = p.first().drop(2).takeWhile { it != ':' }.toInt(),
-                            target = p.last()
-                        )
-                    } else {
-                        Pointer(c)
-                    }
+            val rules = rest.split(",").toMutableList()
+            val fallback = Fallback(rules.removeLast())
+
+            val q = rules
+                .map { r ->
+                    val p = r.split(':')
+
+                    Rule(
+                        param = p.first().first(),
+                        op = p.first()[1],
+                        value = p.first().drop(2).takeWhile { it != ':' }.toLong(),
+                        target = p.last()
+                    )
                 }
 
-            w.first() to q
+            name to (q to fallback)
         }
 
-    var res = 0L
+    fun calculate(rng: Map<Char, LongRange>, name: String): Long {
+        var ranges = rng.toMutableMap()
 
-    val visited = mutableSetOf<Condition>()
-    val stack = Stack<String>()
-    stack.push("in")
-
-    wh@ while (stack.isNotEmpty()) {
-        val level = stack.peek()
-        fo@ for (c in input[level]!!) {
-            if (c is NormalCondition && c.target == "A") {
-                // inc
-                continue@fo
-            }
-
-            if (c is Pointer && c.target == "A") {
-                // inc
-                continue@fo
-            }
-
-
-            if (c is NormalCondition && c.target == "R") {
-                continue@fo
-            }
-
-            if (c is Pointer && c.target == "R") {
-                continue@fo
-            }
-
-            if (c in visited) {
-                continue
-            }
-
-            visited.add(c)
-
-            stack.push(
-                when (c) {
-                    is NormalCondition -> c.target
-                    is Pointer -> c.target
-                }
-            )
-
-            break@fo
+        if (name == "R") {
+            return 0
         }
+
+        if (name == "A") {
+            return ranges.values.fold(1) { acc, range ->
+                acc * (range.last - range.first + 1)
+            }
+        }
+
+        val (rules, fallback) = input[name]!!
+
+        var total = 0L
+
+        var broke = false
+        for (rule in rules) {
+            val l = ranges[rule.param]!!.first
+            val h = ranges[rule.param]!!.last
+
+            val truePart = if (rule.op == '<') {
+                l..min(h, rule.value - 1)
+            } else {
+                max(l, rule.value + 1)..h
+            }
+
+            val falsePart = if (rule.op == '<') {
+                max(l, rule.value)..h
+            } else {
+                l..min(h, rule.value)
+            }
+
+            if (truePart.first <= truePart.last) {
+                val copy = ranges.toMutableMap()
+                copy[rule.param] = truePart
+                total += calculate(copy, rule.target)
+            }
+
+            if (falsePart.first <= falsePart.last) {
+                ranges = ranges.toMutableMap()
+                ranges[rule.param] = falsePart
+            } else {
+                broke = true
+                break
+            }
+        }
+
+        if (!broke) {
+            total += calculate(ranges, fallback.target)
+        }
+
+        return total
     }
 
-    res.println()
+    calculate("xmas".associate { it to 1L..4000L }, "in")
+        .println()
 }
 
 fun main() {
