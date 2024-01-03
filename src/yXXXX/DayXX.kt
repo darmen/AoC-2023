82801e373002a1713daa185b00d7277565796159
Darmen Amanbayev <darmen.amanbayev@gmail.com>
2024-01-03 18:19:29 +0100

    Add template for class-based solutions

---
+++ src/yXXXX/DayXX.kt
@@ -0,0 +1,17 @@
+package yXXXX
+
+import readInput
+
+class DayXX(input: List<String>) {
+    fun solve1(): Any {
+        return 1
+    }
+
+    fun solve2(): Any {
+        return 1
+    }
+}
+
+fun main() {
+    val day = DayXX(readInput())
+}
