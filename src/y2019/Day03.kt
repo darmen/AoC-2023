469caa99328954f72453398903e437cb6650f562
Darmen Amanbayev <darmen.amanbayev@gmail.com>
2024-01-04 22:47:05 +0100

    y2019 d3 p2

--- src/y2019/Day03.kt
+++ src/y2019/Day03.kt
@@ -4,12 +4,12 @@ import Direction
 import Point2D
 import println
 import readInput
+import runMeasure
 
 class Day03(val input: List<String>) {
-    private val wire1 = buildGrid(input.first())
-    private val wire2 = buildGrid(input.last())
 
-    private fun buildGrid(wire: String): Set<Point2D> = buildSet {
+
+    private fun buildGridPart1(wire: String): Set<Point2D> = buildSet {
         var point = Point2D(0, 0)
         this.add(point)
 
@@ -31,7 +31,35 @@ class Day03(val input: List<String>) {
         }
     }
 
+    private fun buildGridPart2(wire: String): Map<Int, Point2D> = buildMap {
+        var point = Point2D(0, 0)
+        this[0] = point
+
+        wire.split(",").forEach {
+            val steps = it.drop(1).toInt()
+
+            val direction = when (it.first()) {
+                'R' -> Direction.RIGHT
+                'L' -> Direction.LEFT
+                'U' -> Direction.UP
+                'D' -> Direction.DOWN
+                else -> throw IllegalArgumentException("Unrecognized direction")
+            }
+
+            for (i in 1..steps) {
+                point = Point2D(point.x + direction.d.first, point.y + direction.d.second)
+
+                if (!this.containsValue(point)) {
+                    this[this.keys.size] = point
+                }
+            }
+        }
+    }
+
     fun solve1(): Int {
+        val wire1 = buildGridPart1(input.first())
+        val wire2 = buildGridPart1(input.last())
+
         return wire1
             .intersect(wire2)
             .filter { it != Point2D(0, 0) }
@@ -41,13 +69,23 @@ class Day03(val input: List<String>) {
     }
 
     fun solve2(): Any {
-        TODO("Not implemented yet")
+        val wire1 = buildGridPart2(input.first())
+        val wire2 = buildGridPart2(input.last())
+
+        val intersections = wire1.values
+            .intersect(wire2.values.toSet())
+            .filterNot { it == Point2D(0, 0) }
+
+        return intersections.minOfOrNull {
+            wire1.filterValues { v -> v == it }.keys.first() + wire2.filterValues { v -> v == it }.keys.first()
+        }!!
     }
 }
 
 fun main() {
-    val day = Day03(readInput())
-
-    day.solve1().println()
-    day.solve2().println()
+    runMeasure {
+        val day = Day03(readInput())
+        day.solve1().println()
+        day.solve2().println()
+    }
 }
