3a5f02a9afebd47538c0566d3fbce9161e9d8b47
Darmen Amanbayev <darmen.amanbayev@gmail.com>
2024-01-05 21:23:48 +0100

    y2019 d8 p2

--- src/y2019/Day08.kt
+++ src/y2019/Day08.kt
@@ -18,8 +18,24 @@ class Day08(val input: List<String>) {
         return layer.count { it == '1' } * layer.count { it == '2' }
     }
 
-    fun solve2(): Int {
-        TODO("Not implemented yet")
+    fun solve2(): String {
+        val layers = input.first().chunked(width * height)
+        val topLayer = mutableListOf<Char>()
+
+        for (i in 0..<layers.first().length) {
+            for (j in layers.indices) {
+                if (layers[j][i] == '2') {
+                    continue
+                } else {
+                    topLayer.add(layers[j][i])
+                    break
+                }
+            }
+        }
+
+        return topLayer.joinToString("")
+            .chunked(width)
+            .joinToString("\n")
     }
 }
 
