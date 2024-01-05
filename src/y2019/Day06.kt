ac3b648d5b088efb7f92d8b3b2e70f369e8ef466
Darmen Amanbayev <darmen.amanbayev@gmail.com>
2024-01-05 17:27:44 +0100

    y2019 d6 p2

--- src/y2019/Day06.kt
+++ src/y2019/Day06.kt
@@ -1,5 +1,6 @@
 package y2019
 
+import edu.uci.ics.jung.algorithms.shortestpath.UnweightedShortestPath
 import edu.uci.ics.jung.graph.DirectedSparseGraph
 import edu.uci.ics.jung.graph.util.EdgeType
 import println
@@ -9,7 +10,7 @@ import runMeasure
 class Day06(val input: List<String>) {
     private val graph = DirectedSparseGraph<String, String>()
 
-    fun solve1(): Int {
+    init {
         for (s in input) {
             val a = s.split(")").first()
             val b = s.split(")").last()
@@ -19,7 +20,9 @@ class Day06(val input: List<String>) {
 
             graph.addEdge("$b -> $a", b, a, EdgeType.DIRECTED)
         }
+    }
 
+    fun solve1(): Int {
         var r = 0
         for (vertex in graph.vertices.filterNot { it == "COM" }) {
             var v = vertex
@@ -33,7 +36,22 @@ class Day06(val input: List<String>) {
     }
 
     fun solve2(): Int {
-        TODO("Not implemented yet")
+        val you = graph.getSuccessors("YOU").first()
+        val san = graph.getSuccessors("SAN").first()
+        val usp = UnweightedShortestPath(graph)
+
+        return graph.vertices
+            .mapNotNull { v ->
+                val distanceYou = usp.getDistance(you, v)
+                val distanceSan = usp.getDistance(san, v)
+
+                if (distanceYou == null || distanceSan == null) {
+                    return@mapNotNull null
+                }
+
+                distanceYou.toInt() + distanceSan.toInt()
+            }
+            .min()
     }
 }
 
