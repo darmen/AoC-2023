b4c7f5177c56474400e477d0ac8325af16190268
Darmen Amanbayev <darmen.amanbayev@gmail.com>
2024-12-13 22:02:12 +0100

    feat: day13 part 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,19 @@
 import com.heydarmen.adventofcode.println
 import com.heydarmen.adventofcode.readInput
 import com.heydarmen.adventofcode.runMeasure
-import com.heydarmen.adventofcode.y2024.Day09
+import com.heydarmen.adventofcode.y2024.Day13
 
 fun main() {
     val input = readInput().lines()
 
     runMeasure {
-        Day09(input)
+        Day13(input)
             .solvePart1()
             .println()
     }
 
     runMeasure {
-        Day09((input))
+        Day13((input))
             .solvePart2()
             .println()
     }
