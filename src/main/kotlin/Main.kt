bbf38e763428de4906bc7f0ade944d77cbd9b8d2
Darmen Amanbayev <darmen.amanbayev@gmail.com>
2024-12-14 09:17:05 +0100

    Revert "feat: day13 part 1"
    This reverts commit b4c7f5177c56474400e477d0ac8325af16190268.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,19 @@
 import com.heydarmen.adventofcode.println
 import com.heydarmen.adventofcode.readInput
 import com.heydarmen.adventofcode.runMeasure
-import com.heydarmen.adventofcode.y2024.Day13
+import com.heydarmen.adventofcode.y2024.Day09
 
 fun main() {
     val input = readInput().lines()
 
     runMeasure {
-        Day13(input)
+        Day09(input)
             .solvePart1()
             .println()
     }
 
     runMeasure {
-        Day13((input))
+        Day09((input))
             .solvePart2()
             .println()
     }
